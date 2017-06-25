ff5a96ca111aacf0cf1074269b0c6611b693cc02
Howell <th15817161961@gmail.com>
2017-06-25 22:55:14 +0800

    start

--- src/main/java/cn/com/alien/controller/IndexController.java
+++ src/main/java/cn/com/alien/controller/IndexController.java
@@ -1,11 +1,16 @@
 package cn.com.alien.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Root on 2017/6/25.
@@ -13,23 +18,22 @@ import java.io.IOException;
 @RestController
 public class IndexController {
 
+    @Autowired
+    DiscoveryClient client;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public void index(HttpServletResponse response) {
         try {
-            response.getWriter().print("Hi!");
+            List<ServiceInstance> list = client.getInstances("cloudServer");
+            for (ServiceInstance s:
+                 list) {
+                response.getWriter().print(s.getUri());
+                response.getWriter().print((new RestTemplate()).getForObject(s.getUri(),String.class));
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    @RequestMapping(value = "/info", method = RequestMethod.GET)
-    public void info(HttpServletResponse response) {
-        try {
-            response.getWriter().print("Hi!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
 
 }
