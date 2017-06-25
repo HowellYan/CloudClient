a32d4b288c5bcc6f0e9720ad29d21b6d4d5f3a8f
Howell <th15817161961@gmail.com>
2017-06-26 00:00:27 +0800

    start

--- src/main/java/cn/com/alien/controller/IndexController.java
+++ src/main/java/cn/com/alien/controller/IndexController.java
@@ -3,6 +3,10 @@ package cn.com.alien.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +14,9 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Root on 2017/6/25.
@@ -25,10 +31,22 @@ public class IndexController {
     public void index(HttpServletResponse response) {
         try {
             List<ServiceInstance> list = client.getInstances("cloudServer");
+            List<String> listServices = client.getServices();
+            for (String s:
+                    listServices) {
+                response.getWriter().println(s);
+            }
             for (ServiceInstance s:
                  list) {
-                response.getWriter().print(s.getUri());
-                response.getWriter().print((new RestTemplate()).getForObject(s.getUri(),String.class));
+                response.getWriter().println(s.getUri());
+                response.getWriter().print((new RestTemplate()).getForObject(s.getUri()+"/info",String.class));
+                RestTemplate restTemplate = new RestTemplate();
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("name","abc");
+//                RequestEntity<Map> mapRequestEntity = new RequestEntity<Map>(map, HttpMethod.POST, s.getUri());
+//
+//                ResponseEntity<String> resp = restTemplate.exchange(s.getUri()+"/userLogin",HttpMethod.POST, mapRequestEntity, String.class);
+//                response.getWriter().println(resp.getBody());
             }
 
         } catch (IOException e) {
