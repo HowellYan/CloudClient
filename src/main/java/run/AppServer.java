b45a6911f2a83b0b04d5b10e54cfe304a2c1dff0
HowellYang <th15817161961@gmail.com>
2018-07-19 13:26:32 +0800

    consul

--- src/main/java/run/AppServer.java
+++ src/main/java/run/AppServer.java
@@ -1,30 +1,51 @@
 package run;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by HowellYang on 19/6/17 PM2:27.
  * E-Mail:th15817161961@gmail.com
  */
 @ComponentScan("cn.com.alien")
-@Configuration
-@EnableAutoConfiguration
+@EnableDiscoveryClient
 @RestController
-@EnableConfigurationProperties
-@EnableFeignClients
+@SpringBootApplication
 public class AppServer {
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    //开启软均衡负载
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    /**
+     * 获取所有服务
+     */
+    @RequestMapping("/services")
+    public Object services() {
+        return discoveryClient.getServices();
+    }
+
     @RequestMapping("/")
     public String home() {
-        return "Hello World";
+        return this.restTemplate.getForObject("http://CloudServer/home", String.class);
     }
 
     public static void main(String[] args) {
